6a65e1c8ebddcc7377b1b8c861cd3a6a6850a601
Gaurav Kumar <rickgaurav@gmail.com>
2018-10-30 19:14:23 -0700

    first commit

---
+++ src/RightBody.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class RightBody extends Component{
+    render() {
+        return (
+            <div className ='body-child'>
+                whattttt
+            </div>
+        );
+
+            
+    }
+}
